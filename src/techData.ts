e588824eaf77931e4a754f87b16f4aeaa2f7ac27
willbraun <williamhbraun1@gmail.com>
2023-07-06 08:50:50 -0400

    added skills

--- src/techData.ts
+++ src/techData.ts
@@ -1,18 +1,25 @@
 import react from '/src/assets/images/tech-logos/react-logo.png?w=150&format=avif;webp;png&picture';
+import redux from '/src/assets/images/tech-logos/redux-logo-1.png?w=150&format=avif;webp;png&picture';
 import svelte from '/src/assets/images/tech-logos/svelte-logo.png?w=150&format=avif;webp;png&picture';
 import typescript from '/src/assets/images/tech-logos/typescript-logo.png?w=150&format=avif;webp;png&picture';
 import html from '/src/assets/images/tech-logos/html-logo.png?w=150&format=avif;webp;png&picture';
 import css from '/src/assets/images/tech-logos/css-logo.png?w=150&format=avif;webp;png&picture';
+import scss from '/src/assets/images/tech-logos/scss-logo.png?w=150&format=avif;webp;png&picture';
 import bootstrap from '/src/assets/images/tech-logos/bootstrap-logo.png?w=125&h=100&format=avif;webp;png&picture';
 import python from '/src/assets/images/tech-logos/python-logo.png?w=150&format=avif;webp;png&picture';
 import django from '/src/assets/images/tech-logos/django-logo.png?w=150&format=avif;webp;png&picture';
 import perl from '/src/assets/images/tech-logos/perl-logo.png?w=150&format=avif;webp;png&picture';
 import git from '/src/assets/images/tech-logos/git-logo.png?w=150&format=avif;webp;png&picture';
+import linux from '/src/assets/images/tech-logos/linux-logo.png?w=150&format=avif;webp;png&picture';
 
 export const techData = [
 	{
 		name: 'React',
 		image: react
+	},
+	{
+		name: 'Redux',
+		image: redux
 	},
 	{
 		name: 'Svelte',
@@ -29,6 +36,10 @@ export const techData = [
 	{
 		name: 'CSS',
 		image: css
+	},
+	{
+		name: 'SCSS',
+		image: scss
 	},
 	{
 		name: 'Bootstrap',
@@ -49,5 +60,9 @@ export const techData = [
 	{
 		name: 'Git',
 		image: git
+	},
+	{
+		name: 'Linux',
+		image: linux
 	}
 ];
