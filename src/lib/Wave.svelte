0707c9357f9e51ff6df0170a220a8420eac5a087
willbraun <williamhbraun1@gmail.com>
2025-07-22 16:37:48 -0400

    added blur to waves

--- src/lib/Wave.svelte
+++ src/lib/Wave.svelte
@@ -12,6 +12,12 @@
 				id="gentle-wave"
 				d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
 			/>
+			<filter id="wave-blur" x="-50%" y="-50%" width="200%" height="200%">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="0.3" />
+			</filter>
+			<filter id="wave-blur-strong" x="-50%" y="-50%" width="200%" height="200%">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="0.5" />
+			</filter>
 		</defs>
 		<linearGradient id="Gradient" x1="0" x2="0" y1="0" y2="1">
 			<stop offset="0%" stop-color="rgba(0,180, 250,1)" />
@@ -19,9 +25,30 @@
 			<stop offset="100%" stop-color="rgba(0,180, 250,1)" />
 		</linearGradient>
 		<g class="parallax">
-			<use xlink:href="#gentle-wave" x="48" y="0" fill="url(#Gradient)" opacity="0.7" />
-			<use xlink:href="#gentle-wave" x="48" y="3" fill="url(#Gradient)" opacity="0.5" />
-			<use xlink:href="#gentle-wave" x="48" y="5" fill="url(#Gradient)" opacity="0.3" />
+			<use
+				xlink:href="#gentle-wave"
+				x="48"
+				y="0"
+				fill="url(#Gradient)"
+				opacity="0.7"
+				filter="url(#wave-blur)"
+			/>
+			<use
+				xlink:href="#gentle-wave"
+				x="48"
+				y="3"
+				fill="url(#Gradient)"
+				opacity="0.5"
+				filter="url(#wave-blur)"
+			/>
+			<use
+				xlink:href="#gentle-wave"
+				x="48"
+				y="5"
+				fill="url(#Gradient)"
+				opacity="0.3"
+				filter="url(#wave-blur-strong)"
+			/>
 			<use xlink:href="#gentle-wave" x="48" y="7" fill="rgba(0,28,85,1)" opacity="1" />
 		</g>
 	</svg>
