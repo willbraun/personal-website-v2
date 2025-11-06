34092b1a5c5a7de0bf027a81ae698354a7ccb6d8
willbraun <williamhbraun1@gmail.com>
2025-11-06 07:31:44 -0500

    update to svelte 4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,17 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+			rules: {
+				'no-undef': 'off', // Disabled for Svelte files as it conflicts with Svelte syntax
+				'@typescript-eslint/no-unused-vars': 'off', // Disabled for Svelte files as imports are used in templates
+				'no-unused-vars': 'off' // Also disable the base ESLint version
+			}
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
@@ -16,5 +26,9 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'no-undef': 'off', // Let TypeScript handle undefined variables
+		'missing-declaration': 'off' // Disabled globally - false positives for Svelte components and variables
 	}
 };
